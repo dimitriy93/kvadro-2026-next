fcf640e0aaa646ad7c78de3a8e0406cef2b6ee01
Dimitriy Ivanov <dimitriy.ivanov@bask.ws>
2026-04-26 11:20:54 +0300

    Add 404 not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import notFoundImage from "@/app/_assets/images/not_found.webp";
 import {Metadata} from "next";
+import {CopyrightBar} from "@/components/CopyrightBar";
 import {Button} from "@/components/Button";
 import {not_found} from "@/config/content/not_found";
 import "@/styles/not_found.scss";
@@ -30,6 +31,7 @@ export default function NotFound() {
                     </Link>
                 </div>
             </div>
+            <CopyrightBar />
         </div>
     )
 }
